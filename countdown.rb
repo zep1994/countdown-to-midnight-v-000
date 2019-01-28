4a53421871b02558b7c8ce3dde4ee74b0283cc26
Thomas Wesley Matlock <snaxbags@protonmail.com>
2019-01-28 18:51:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,11 @@ def countdown(num)
   while num>0
     puts "#{num} SECOND(S)!"
     num -= 1
-    countdown_with_sleep(1)
+    sleep 1
   end
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num)
-  sleep num
-end
+# def countdown_with_sleep(num)
+#   sleep num
+# end
